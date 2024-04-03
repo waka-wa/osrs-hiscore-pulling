0cca3b68ce466444ae65ee0c908718587a2df3a5
waka____ <12706753+waka-wa@users.noreply.github.com>
2024-04-03 14:37:39 -0700

    username fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://waka-waka-waka-waka.github.io');
+  res.header('Access-Control-Allow-Origin', 'https://waka-wa.github.io');
   res.header('Access-Control-Allow-Methods', 'GET');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
